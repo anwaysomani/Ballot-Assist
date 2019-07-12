26a3e44a1e85ec3328067e1ad4cc20ebdb3a55b7
Anway Somani <anwaysomani@gmail.com>
2019-07-12 14:18:01 +0530

    Activated test-server for app testing

--- ballotassist/urls.py
+++ ballotassist/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from ballotassist import views as ballotviews
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'', ballotviews.main, name='homepage'),
 ]
