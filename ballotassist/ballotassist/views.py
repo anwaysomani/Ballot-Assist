cdaf1aa3e9df6289c6223df2fca0a176dfae724a
Anway Somani <anwaysomani@gmail.com>
2019-07-12 15:29:33 +0530

    Added proper length record for records_list

--- ballotassist/ballotassist/views.py
+++ ballotassist/ballotassist/views.py
@@ -1,6 +1,13 @@
 from __future__ import unicode_literals
 from django.shortcuts import render
+from .models import ballot_record
 
 def main(request):
-    return render(request, 'index.html', {})
+    records = ballot_record.objects.all()
+
+    context = {
+        'records': records,
+    }
+
+    return render(request, 'index.html', context)
 
