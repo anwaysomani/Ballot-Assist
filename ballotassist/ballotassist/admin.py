66ea3887c5c50d935f4c2da0711c0907ff0bc414
Anway Somani <anwaysomani@gmail.com>
2019-07-12 15:30:01 +0530

    Site-register for ballot_record

--- ballotassist/ballotassist/admin.py
+++ ballotassist/ballotassist/admin.py
@@ -1,6 +1,6 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.contrib import admin
+from .models import ballot_record
 
-# Register your models here.
+admin.site.register(ballot_record)
+
